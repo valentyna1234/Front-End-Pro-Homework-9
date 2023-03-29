29d8ea78d6d6f83242800d4d0dd14691d30b96fb
valentyna1234 <pisareva.valya@gmail.com>
2023-03-29 13:41:12 +0300

    Add index.js with hw 9.1

---
+++ index-1.js
@@ -0,0 +1,15 @@
+//Вивести на сторінку в один рядок через кому числа від 10 до 20.
+
+let result = '';
+
+for ( let i = 10; i <= 20; i = i + 1 ) {
+    result = result + i +',';
+}
+console.log('Task 9.1: ' + result);
+
+
+
+
+
+
+
