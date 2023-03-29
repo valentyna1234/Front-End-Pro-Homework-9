f46fe5802c7efb81ad01240e3cbb1c8ca5d5af86
valentyna1234 <pisareva.valya@gmail.com>
2023-03-29 13:49:40 +0300

    Add index.js with hw 9.10

---
+++ index-10.js
@@ -0,0 +1,11 @@
+//Визначити кількість його парних дільників.
+
+console.log('Task 9.10:');
+
+let numNext = 120
+
+for (let i = 1; i <= numNext; i++) {
+    if (numNext % i === 0)
+    if (i % 2 === 0)
+    console.log(i);
+}
